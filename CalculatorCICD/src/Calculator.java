71a04bcf4eaacb83375070f33e1d7455d750dda3
briana.wynn@revature.net <briana.wynn@revature.net>
2022-01-06 11:01:05 -0800

    initial commit

---
+++ CalculatorCICD/src/Calculator.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		double fnum, snum, answer;
+		
+		System.out.println("Enter first num: ");
+		fnum = scanner.nextDouble();
+		
+		System.out.println("Enter second num: ");
+		snum = scanner.nextDouble();
+		
+		answer = fnum + snum;
+		System.out.println(answer);
+	}
+
+}
