ab6f8d660ac403d271c874f284a40ccdfdd5af66
briana.wynn@revature.net <briana.wynn@revature.net>
2022-01-06 11:19:41 -0800

    add the option to select different operator

--- CalculatorCICD/src/Calculator.java
+++ CalculatorCICD/src/Calculator.java
@@ -5,9 +5,13 @@ public class Calculator {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		double fnum, snum, answer;
+		String operator;
 		
 		System.out.println("Enter first num: ");
 		fnum = scanner.nextDouble();
+		
+		System.out.println("Enter operator");
+		operator = scanner.nextLine();
 		
 		System.out.println("Enter second num: ");
 		snum = scanner.nextDouble();
